2230df4ef0942be5b67e2111c60b1e9aacbb7c40
steve <steve@blossm.io>
2018-04-14 17:47:49 -0400

    add overrides

--- cli.js
+++ cli.js
@@ -5,7 +5,45 @@ var path = require('path')
 
 var argv = require('minimist')(process.argv.slice(2))
 var dotenv = require('dotenv')
-var dotenvExpand = require('dotenv-expand')
+
+var dotenvExpand = function (config, override) {
+    var interpolate = function (env) {
+      var matches = env.match(/\$([a-zA-Z0-9_]+)|\${([a-zA-Z0-9_]+)}/g) || []
+  
+      matches.forEach(function (match) {
+        var key = match.replace(/\$|{|}/g, '')
+  
+        // process.env value 'wins' over .env file's value
+        var variable = process.env[key] || config.parsed[key] || ''
+  
+        // Resolve recursive interpolations
+        variable = interpolate(variable)
+  
+        env = env.replace(match, variable)
+      })
+  
+      return env
+    }
+  
+    for (var configKey in config.parsed) {
+      var value = (!override) ? process.env[configKey] || config.parsed[configKey] : config.parsed[configKey] || process.env[configKey]
+  
+      if (config.parsed[configKey].substring(0, 2) === '\\$') {
+        config.parsed[configKey] = value.substring(1)
+      } else if (config.parsed[configKey].indexOf('\\$') > 0) {
+        config.parsed[configKey] = value.replace(/\\\$/g, '$')
+      } else {
+        config.parsed[configKey] = interpolate(value)
+      }
+    }
+  
+    for (var processKey in config.parsed) {
+      process.env[processKey] = config.parsed[processKey]
+    }
+  
+    return config
+  }
+  
 
 var paths = ['.env']
 if (argv.e) {
@@ -16,11 +54,13 @@ if (argv.e) {
   }
 }
 paths.forEach(function (env) {
-    dotenvExpand(dotenv.config({path: path.resolve(env)}))
+    dotenvExpand(dotenv.config({path: path.resolve(env)}), true)
 })
 
+
 if (argv.p) {
-  console.log(process.env[argv.p])
+    console.log(process.env);
+//   console.log(process.env[argv.p])
   process.exit()
 }
 
